3f6d8b2ad2705cb5294b65e74a9e066b63f16b8e
som1chan <31833556+som1chan@users.noreply.github.com>
2020-11-16 00:51:39 -0600

    Bot stat uptime visual changes

--- packages/back-end/plugins/Info/commands/BotStats.ts
+++ packages/back-end/plugins/Info/commands/BotStats.ts
@@ -11,14 +11,22 @@ export default class extends BaseCommand {
 		super(plugin, {
 			id: "botstats",
 			name: "Bot Stats",
-			about: "Gets various stats from the bot, including Framed version and uptime.",
+			about:
+				"Gets various stats from the bot, including Framed version and uptime.",
 		});
 	}
 
 	async run(msg: FramedMessage): Promise<boolean> {
 		const framedUser = framedClient.client.user;
 		const discordMsg = msg.discord?.msg;
-		const environment = process.env.NODE_ENV ? ` ${process.env.NODE_ENV}` : "";
+		const environment = process.env.NODE_ENV
+			? ` ${process.env.NODE_ENV}`
+			: "";
+		
+		// For debugging
+		// eslint-disable-next-line prefer-const
+		let uptime = process.uptime();
+		// uptime = 216120;
 
 		if (discordMsg && framedUser) {
 			const codeblock = "```";
@@ -29,12 +37,11 @@ export default class extends BaseCommand {
 			).setDescription(stripIndent`
 				[Framed](https://github.com/som1chan/Framed)
 				${codeblock}
-				Uptime:           ${this.secondsToDhms(process.uptime())}
+				Uptime:           ${this.secondsToDhms(uptime)}
 				Framed Version:   v${msg.framedClient.version}
 				Back-End Version: v${msg.framedClient.backEndVersion}${environment}
 				${codeblock}
-				`
-			);
+				`);
 			discordMsg.channel.send(embed);
 		}
 		return true;
@@ -47,10 +54,21 @@ export default class extends BaseCommand {
 		const m = Math.floor((seconds % 3600) / 60);
 		const s = Math.floor(seconds % 60);
 
-		const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
-		const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-		const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes ") : "";
-		const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-		return dDisplay + hDisplay + mDisplay + sDisplay;
+		// const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+		// const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+		// const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes ") : "";
+		// const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+		const dDisplay = d > 0 ? `${d}d ` : "";
+		const hDisplay = h > 0 ? `${h}hr `: "";
+		const mDisplay = m > 0 ? `${m}m ` : "";
+		const sDisplay = s > 0 ? `${s}s ` : "";
+		
+		// const dDisplay = d > 0 ? `${d}d ` : "";
+		// const hDisplay = h > 0 ? `${h}:`: "";
+		// const mDisplay = m > 0 ? `${m < 10 ? "0" : ""}${m}:` : "00:";
+		// const sDisplay = s > 0 ? `${s < 10 ? "0" : ""}${s}` : "00";
+
+		return `${dDisplay}${hDisplay}${mDisplay}${sDisplay}`;
+		// return dDisplay + hDisplay + mDisplay + sDisplay;
 	}
 }
