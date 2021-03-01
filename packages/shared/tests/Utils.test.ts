a46370032ab62286f9d26a9451c8e01e489da786
some1chan <31833556+some1chan@users.noreply.github.com>
2021-03-01 01:57:23 -0600

    test: change names

--- packages/shared/tests/Utils.test.ts
+++ packages/shared/tests/Utils.test.ts
@@ -1,12 +1,10 @@
 import { Utils } from "../src/index";
 
-test(`Utils.formatHrTime adds an "s" if requested`, () => {
+test(`formatHrTime adds an "s" if requested`, () => {
 	expect(Utils.formatHrTime([0, 123456789], true)).toBe("0.123s");
 });
 
-test(`Utils.formatHrTime handles "0"s padding correctly`, () => {
+test(`formatHrTime handles "0"s padding correctly`, () => {
 	expect(Utils.formatHrTime([0, 12345678])).toBe("0.012");
 	expect(Utils.formatHrTime([0, 1234567])).toBe("0.001");
 });
-
-
