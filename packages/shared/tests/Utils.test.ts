9954a441fe8b70d72512e4798fc36e86c63181b9
some1chan <31833556+some1chan@users.noreply.github.com>
2021-03-01 00:33:25 -0600

    test: better "0"s test

--- packages/shared/tests/Utils.test.ts
+++ packages/shared/tests/Utils.test.ts
@@ -1,12 +1,12 @@
 import { Utils } from "../src/index";
 
-test(`Utils.formatHrTime adds trailing zeros if needed`, () => {
-	const num = 42;
-	const results = Utils.formatHrTime([num, 0]);
-	expect(results).toBe(`${num}.000`);
+test(`Utils.formatHrTime adds an "s" if requested`, () => {
+	expect(Utils.formatHrTime([0, 123456789], true)).toBe("0.123s");
 });
 
-test(`Utils.formatHrTime adds an "s" if requested`, () => {
-	const results = Utils.formatHrTime([0, 123456789], true);
-	expect(results).toBe("0.123s");
+test(`Utils.formatHrTime handles "0"s padding correctly`, () => {
+	expect(Utils.formatHrTime([0, 12345678])).toBe("0.012");
+	expect(Utils.formatHrTime([0, 1234567])).toBe("0.001");
 });
+
+
