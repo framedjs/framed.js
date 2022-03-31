ca6423cf5285d51d711565b01b2a8185da0d6ac2
some1chan <31833556+some1chan@users.noreply.github.com>
2022-03-31 03:53:59 -0500

    refactor!: remove koa and BaseRouter

--- packages/core/src/structures/BaseRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from "./Client";
-import Router from "koa-router";
-
-export abstract class BaseRouter {
-	readonly router = new Router();
-
-	constructor(readonly client: Client) {}
-}
